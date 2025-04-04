db93bd41251dc2994fb3d91ba6a17ef27e64c4ed
birzulele <andrei.birzu@student.upt.ro>
2025-04-04 18:47:10 +0300

    Definire constante necesare

---
+++ 2048.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+# definire constante
+
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLOR = (50, 129, 168)
+OUTLINE_THICKNESS = 12
+BACKGROUND_COLOR = (50, 60, 168)
+FONT_COLOR = (169, 149, 50)
+
